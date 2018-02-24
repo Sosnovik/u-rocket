dd74caab1c69e70dd60eeb5c74becefe1bcbc170
Vera <sosnovikvera@gmail.com>
2018-02-24 18:18:41 +0300

    new files

---
+++ image_gen.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pandas as pd
+
+def gaussian2D(shape, mean, cov):
+    '''
+    add gaussian to source image
+    params:
+        shape - shape of the output
+        mean - array-like tuple of x_mean, y_mean - relative positions
+        cov - 2x2 covariance matrix
+        magnitude - maximal intensity of gaussian
+    '''
+    X, Y = np.indices(shape)
+    X = (X + 1).astype(np.float) / (shape[0] + 1)
+    Y = (Y + 1).astype(np.float) / (shape[1] + 1)
+    r = np.stack([X, Y], -1) - mean
+    exp = np.einsum('ijk,kl,ijl->ij', r, np.linalg.inv(cov), r)
+    exp = np.exp(-0.5 * exp)
+    return exp
+
+def random_small_rotation():
+    angle = np.random.normal(0, scale=0.1)
+    matrix = np.array([
+        [np.cos(angle), np.sin(angle)],
+        [-1.0 * np.sin(angle), np.cos(angle)]
+    ])
+    return matrix
+
+def add_gaussian_on_image(image, magnitude, mean, radius, cov):        
+    new_image = image.copy()
+    gauss = gaussian2D(image.shape, mean, radius * cov)
+    new_image += gauss * magnitude
+    return new_image
+
+def gen_random_images():
+
+    # Background
+    dark_color = np.random.randint(50, 150)
+    light_color = np.random.randint(1, 255 - dark_color)
+    
+    img = get_random_background() * dark_color
+    t_vec = np.random.random(size=2)
+    t_vec = t_vec / np.linalg.norm(t_vec)
+    
+    images = []
+    masks = []
+    
+    
+    point = np.random.random(size=2)
+    radius = np.random.uniform(0.0001, 0.0025)
+    cov = np.eye(2) + 0.5 * np.random.uniform(-1, 1, size=(2, 2))
+    
+    for _ in range(N_IMAGES):
+        generated_img = add_gaussian_on_image(img, light_color, point, radius, cov)
+        images.append(generated_img)
+        
+        mask = add_gaussian_on_image(np.zeros_like(img), 1.0, point, radius, cov) > 0.5
+        mask = mask.astype('float32')
+        masks.append(mask)
+        
+        t_vec = random_small_rotation().dot(t_vec)
+        distance = 0.1 * np.random.normal(1, scale=0.1)
+        point += t_vec * distance
+        cov = random_small_rotation().dot(cov)
+        radius *= np.random.uniform(0.7, 0.9)
+        
+    # White noise
+    noise = np.random.randint(0, 200, size=image_size)
+    p_noise = np.random.random(size=image_size)
+    th = np.random.uniform(0, 0.1)
+    for img in images:
+#         pass
+        img[p_noise < th] = noise[p_noise < th]
+#         new_images.append(np.expand_dims(img[i], -1
+    
+    return np.stack(images, axis=-1), np.stack(masks, axis=-1)
--- model.py
+++ model.py
@@ -1,62 +1,10 @@
-# coding: utf-8
-'''
-    - "ZF_UNET_224" Model based on UNET code from following paper: https://arxiv.org/abs/1505.04597
-    - This model used to get 2nd place in DSTL competition: https://www.kaggle.com/c/dstl-satellite-imagery-feature-detection
-    - For training used DICE coefficient: https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
-    - Input shape for model is 224x224 (the same as for other popular CNNs like VGG or ResNet)
-    - It has 3 input channels (to process standard RGB (BGR) images). You can change it with variable "INPUT_CHANNELS"
-    - It trained on random image generator with random light shapes (ellipses) on dark background with noise (< 10%).
-    - In most cases model ZF_UNET_224 is ok to be used without pretrained weights.
-'''
-
-__author__ = 'ZFTurbo: https://kaggle.com/zfturbo'
-
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers.core import Dropout, Activation
+from keras.layers import *
 from keras import backend as K
-from keras.layers.merge import concatenate
-
-# Number of image channels (for example 3 in case of RGB, or 1 for grayscale images)
-INPUT_CHANNELS = 1
-# Number of output masks (1 in case you predict only one type of objects)
-OUTPUT_MASK_CHANNELS = 1
-
-
-def preprocess_batch(batch):
-    batch /= 256
-    batch -= 0.5
-    return batch
-
-
-def dice_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2.0 * intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1.0)
-
-
-def jacard_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
-
-
-def jacard_coef_loss(y_true, y_pred):
-    return -jacard_coef(y_true, y_pred)
-
-
-def dice_coef_loss(y_true, y_pred):
-    return -dice_coef(y_true, y_pred)
-
+from keras.optimizers import Adam
 
 def double_conv_layer(x, size, dropout, batch_norm):
-    if K.image_dim_ordering() == 'th':
-        axis = 1
-    else:
-        axis = 3
+    axis = 3
     conv = Conv2D(size, (3, 3), padding='same')(x)
     if batch_norm is True:
         conv = BatchNormalization(axis=axis)(conv)
@@ -69,9 +17,8 @@ def double_conv_layer(x, size, dropout, batch_norm):
         conv = Dropout(dropout)(conv)
     return conv
 
-
 def ZF_UNET_224(dropout_val=0.0, batch_norm=True):
-    inputs = Input((128, 128, INPUT_CHANNELS))
+    inputs = Input((512, 512, 3))
     axis = 3
     filters = 16
 
@@ -107,7 +54,7 @@ def ZF_UNET_224(dropout_val=0.0, batch_norm=True):
     up_224 = concatenate([UpSampling2D(size=(2, 2))(up_conv_112), conv_224], axis=axis)
     up_conv_224 = double_conv_layer(up_224, filters, 0, batch_norm)
 
-    conv_final = Conv2D(OUTPUT_MASK_CHANNELS, (1, 1))(up_conv_224)
+    conv_final = Conv2D(3, (1, 1))(up_conv_224)
     conv_final = BatchNormalization(axis=axis)(conv_final)
     conv_final = Activation('sigmoid')(conv_final)
 
