770e9fc3fff9a0df1268ed8147be3abd4a6647ab
Vera <sosnovikvera@gmail.com>
2018-03-17 19:50:09 +0300

    train function

---
+++ rocket_tools/batch_generator.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def batch_generator(batch_size, snr=10.0, th_max=0.5):
+    
+    while True:
+        image_list = []
+        mask_list = []
+        
+        for i in range(batch_size):
+            images, masks = image_generator.get_images()
+            noise_max = np.max(images) / snr
+            noise = np.random.randint(0, noise_max, size=images.shape) 
+            p_noise = np.random.random(size=images.shape)
+            
+            th_noise = np.random.uniform(0, th_max)
+            images[p_noise < th_noise] = noise[p_noise < th_noise]
+            
+            image_list.append(images)
+            mask_list.append(masks)
+            
+
+        image_list = np.array(image_list)
+        image_list = preprocess_batch(image_list)
+        mask_list = np.array(mask_list, dtype=np.float32)
+        yield image_list, mask_list
